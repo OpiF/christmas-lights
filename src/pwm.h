2a6a22ca73a1b00cc5914e24fec90e8cb7548b08
Artur Raczyński <artur.raczynski@gmail.com>
2018-12-10 00:58:43 +0100

    made shorthand for declaring target power draw

--- src/pwm.h
+++ src/pwm.h
@@ -10,4 +10,12 @@ void blink();
 extern const uint16_t targetHigh;
 extern const uint16_t targetLow;
 
+const uint32_t resistorValue = 10; // ohm
+const uint32_t referenceVoltage = 1100; // mV
+
+constexpr uint16_t operator "" _mA(unsigned long long mA)
+{
+    return (mA * resistorValue * 1024) / referenceVoltage;
+}
+
 #endif
